073ba3cca65894af476f8f6c1a9a627022cd98b3
suyong <suyong.lee@samsung.com>
2025-05-16 16:11:02 +0900

    인코딩오류정정

--- CRA_Project_Tester/tester.h
+++ CRA_Project_Tester/tester.h
@@ -22,14 +22,14 @@ class Help : public ITestOperation
 public:
 	void run(string command1 = "", string command2 = "")
 	{
-		cout << "Team Approve : ���ƶ� �̵��� �̼��� �̼��� �̿��� �ѻ��" << endl;
+		cout << "Team Approve : 고아라 이동건 이서영 이수용 이용한 한상민" << endl;
 		cout << "How to use CMD" << endl;
-		cout << "read (address) : (address)�� �����͸� ����" << endl;
-		cout << "write (address) (data) : (address)�� (data)�� ��"<< endl;
-		cout << "fullread : ��� �����͸� ����" << endl;
-		cout << "fullwrite (data) : ��� �ּҿ� (data)�� ��" << endl;
-		cout << "exit : ����" << endl;
-		cout << "help : ����" << endl;
+		cout << "read (address) : (address)의 데이터를 읽음" << endl;
+		cout << "write (address) (data) : (address)에 (data)를 씀" << endl;
+		cout << "fullread : 모든 데이터를 읽음" << endl;
+		cout << "fullwrite (data) : 모든 주소에 (data)를 씀" << endl;
+		cout << "exit : 종료" << endl;
+		cout << "help : 도움말" << endl;
 	}
 
 class Write : public ITestOperation
