82c840eb599bed96bd6732d441d3cf23d6fea9be
yonghan.lee <yonghan.lee@samsung.com>
2025-05-16 14:43:08 +0900

    Init Test

--- CRA_Project_Tester/tester.h
+++ CRA_Project_Tester/tester.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <iostream>
 using namespace std;
 class ITestOperation
@@ -15,4 +15,25 @@ public:
 	{
 		cout << "read" << command << endl;
 	}
+};
+
+class Write : public ITestOperation
+{
+public:
+	void run(string command)
+	{
+		cout << "write" << command << endl;
+	}
+};
+
+class SSDTest :public ITestOperation
+{
+public:
+
+	// ITestOperation을(를) 통해 상속됨
+	void run(string command) override;
+private:
+	void FullWriteAndReadCompare();
+	void PartialLBAWrite();
+	void WriteReadAging();
 };
