1eb9b1850efc974d2243035aeab0319b56816458
Sowon <dsdon9.lee@samsung.com>
2025-05-16 16:15:07 +0900

    [feature] read feature class 생성

--- CRA_Project_Tester/tester.h
+++ CRA_Project_Tester/tester.h
@@ -1,18 +1,20 @@
-#pragma once
+﻿#pragma once
 #include <iostream>
 using namespace std;
 class ITestOperation
 {
 public:
-	virtual void run(string command = "",string command2 = "") = 0;
+	virtual string run(string command = "",string command2 = "") = 0;
 };
 
 //example
 class Read : public ITestOperation
 {
 public:
-	void run(string command1 = "",string command2 = "")
+	string run(string command1 = "",string command2 = "")
 	{
-		cout << "read" << command1 << command2 <<endl;
+		cout << "read" << command1 <<endl;
+		return "hello read";
 	}
 };
+
