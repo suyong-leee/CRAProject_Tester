fe58140da96008878e5cfd5d8b8c74ee6e9aca5b
Sowon <119ldk@naver.com>
2025-05-19 10:39:42 +0900

    read 기능 시 입력 매개변수 예외 처리 testcase 생성

--- CRA_Project_Tester/featureTEST.cpp
+++ CRA_Project_Tester/featureTEST.cpp
@@ -10,16 +10,36 @@ using namespace std;
 
 class MockReadDriver : public Read {
 public:
-    MOCK_METHOD(string, Read, (string cmd1), ());
+	MOCK_METHOD(string, Read, (string cmd1), ());
 
 };
 
 
-
 TEST(SSDTEST, ReadNone) {
-    
-    MockReadDriver mockDriver;
-    EXPECT_CALL(mockDriver, Read("3")).WillRepeatedly(Return("helloWorld"));
-    //cout << mockDriver.Read("3") << endl;
-    EXPECT_EQ(mockDriver.Read("3"), "helloWorld");
+
+	MockReadDriver mockDriver;
+	EXPECT_CALL(mockDriver, Read("3")).WillRepeatedly(Return("helloWorld"));
+	//cout << mockDriver.Read("3") << endl;
+	EXPECT_EQ(mockDriver.Read("3"), "helloWorld");
+}
+
+TEST(SSDTEST, LSBinvalid) {
+
+	MockReadDriver mockDriver;
+	EXPECT_NO_THROW({
+		bool result = mockDriver.checkCMD("12");
+		EXPECT_TRUE(result);
+		});
+}
+
+TEST(SSDTEST, LSBinvalid2) {
+
+	MockReadDriver mockDriver;
+	EXPECT_THROW(mockDriver.checkCMD("123"), std::invalid_argument);
+}
+
+TEST(SSDTEST, LSBinvalid3) {
+
+	MockReadDriver mockDriver;
+	EXPECT_THROW(mockDriver.checkCMD("1A"), std::invalid_argument);
 }
