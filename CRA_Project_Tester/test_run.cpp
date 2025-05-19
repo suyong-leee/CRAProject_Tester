c90bf1324a4981fdf5768dbd71dddbbc5095d8de
yonghanlee <yonghanlee@samsung.com>
2025-05-19 13:58:53 +0900

    [feature]FullWriteAndRead Pass/Fail Case 추가

--- CRA_Project_Tester/test_run.cpp
+++ CRA_Project_Tester/test_run.cpp
@@ -14,17 +14,31 @@ public:
 class MockReadDriver_ : public Read {
 public:
     MOCK_METHOD(void, run, (std::string, std::string), (override));
-    MOCK_METHOD(void, read, (std::string address), ());
+    MOCK_METHOD(string, read, (std::string address), ());
 };
+
+TEST(tester, FullWriteAndReadCompareFailTest)
+{
+    MockWriteDriver_ mockWrite;
+    MockReadDriver_ mockRead;
+
+    EXPECT_CALL(mockWrite, run(_, _)).Times(AnyNumber());
+    EXPECT_CALL(mockRead, read(_)).Times(AnyNumber()).WillOnce(Return("0x12341234"));
+
+    SSDTest_FullWriteAndReadCompare ssd(&mockWrite, &mockRead);
+    
+    EXPECT_THROW(ssd.run("", ""), exception);
+}
 
 TEST(tester, FullWriteAndReadCompareTest)
 {
     MockWriteDriver_ mockWrite;
     MockReadDriver_ mockRead;
 
-    EXPECT_CALL(mockWrite, run(_, _)).Times(100);
-    EXPECT_CALL(mockRead, run(_, _)).Times(100);
+    EXPECT_CALL(mockWrite, run(_, _)).Times(AnyNumber());
+    EXPECT_CALL(mockRead, read(_)).Times(AnyNumber()).WillRepeatedly(Return("0x11111111"));
 
     SSDTest_FullWriteAndReadCompare ssd(&mockWrite, &mockRead);
-    ssd.run("","");
+
+    ssd.run("", "");
 }
