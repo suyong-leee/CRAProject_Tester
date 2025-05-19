0ab9c29ceebd3f867b8ca7d30fb6f2c4b890a9e0
yonghanlee <yonghanlee@samsung.com>
2025-05-19 14:43:34 +0900

    [refact] Fixture Refactoring 적용

--- CRA_Project_Tester/test_run.cpp
+++ CRA_Project_Tester/test_run.cpp
@@ -17,28 +17,35 @@ public:
     MOCK_METHOD(string, read, (std::string address), ());
 };
 
-TEST(tester, FullWriteAndReadCompareFailTest)
-{
+class SSDTestFixture : public ::testing::Test {
+protected:
     MockWriteDriver_ mockWrite;
     MockReadDriver_ mockRead;
+    SSDTest_FullWriteAndReadCompare* ssd;
 
-    EXPECT_CALL(mockWrite, run(_, _)).Times(AnyNumber());
-    EXPECT_CALL(mockRead, read(_)).Times(AnyNumber()).WillOnce(Return("0x12341234"));
+    void SetUp() override {
+        ssd = new SSDTest_FullWriteAndReadCompare(&mockWrite, &mockRead);
+    }
 
-    SSDTest_FullWriteAndReadCompare ssd(&mockWrite, &mockRead);
-    
-    EXPECT_THROW(ssd.run("", ""), exception);
+    void TearDown() override {
+        delete ssd;
+    }
+};
+
+TEST_F(SSDTestFixture, FullWriteAndReadCompareFailTest) {
+    EXPECT_CALL(mockWrite, run(_, _)).Times(::testing::AnyNumber());
+    EXPECT_CALL(mockRead, read(_))
+        .Times(::testing::AnyNumber())
+        .WillOnce(::testing::Return("0x12341234"));
+
+    EXPECT_THROW(ssd->run("", ""), exception);
 }
 
-TEST(tester, FullWriteAndReadCompareTest)
-{
-    MockWriteDriver_ mockWrite;
-    MockReadDriver_ mockRead;
+TEST_F(SSDTestFixture, FullWriteAndReadCompareTest) {
+    EXPECT_CALL(mockWrite, run(_, _)).Times(::testing::AnyNumber());
+    EXPECT_CALL(mockRead, read(_))
+        .Times(::testing::AnyNumber())
+        .WillRepeatedly(Return("0x11111111"));
 
-    EXPECT_CALL(mockWrite, run(_, _)).Times(AnyNumber());
-    EXPECT_CALL(mockRead, read(_)).Times(AnyNumber()).WillRepeatedly(Return("0x11111111"));
-
-    SSDTest_FullWriteAndReadCompare ssd(&mockWrite, &mockRead);
-
-    ssd.run("", "");
+    ssd->run("", "");
 }
