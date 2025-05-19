09d18b03136047c1ab3e0e88bf85cba2b0d86bac
yonghanlee <yonghanlee@samsung.com>
2025-05-19 13:58:14 +0900

    [feature] FullWrite 함수의 ReadCompare기능 구현하여 read Fail시 Exception 발생

--- CRA_Project_Tester/tester.cpp
+++ CRA_Project_Tester/tester.cpp
@@ -1,5 +1,6 @@
 ﻿#include "tester.h"
 #include<string>
+#include<stdexcept>
 using namespace std;
 void SSDTest_FullWriteAndReadCompare::run(string command1, string command2)
 {
@@ -10,13 +11,23 @@ void SSDTest_FullWriteAndReadCompare::run(string command1, string command2)
 	//• 10 ~14번 LBA까지 다른 값으로 Write 명령어를 수행한다.
 	//• 10 ~14번 LBA까지 ReadCompare 수행
 	//• 위와 같은 규칙으로 전체 영역에 대해 Full Write + Read Compare를 수행한다.
+	string writeBuffer[100];
+	string readBuffer;
+	string buffer = "0x11111111";
 	for (int i = 0; i < 100; i += 5)
 	{
 		for (int j = i; j < i + 5; j++) {
-			write->run(to_string(i));
+			write->run(to_string(j),buffer);
+			writeBuffer[j] = buffer;
 		}
 		for (int j = i; j < i + 5; j++) {
-			read->run(to_string(i));
+			readBuffer = read->read(to_string(j));
+			if (writeBuffer[j] == readBuffer) {
+				continue;
+			}
+			else {
+				throw exception("Compare Failed\n");
+			}
 		}
 	}
 
