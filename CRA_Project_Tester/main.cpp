926d21401b899faeea12c2da1394e3d1b7feae27
donggeon9999 <75352014+donggeon9999@users.noreply.github.com>
2025-05-19 12:45:27 +0900

    Update main.cpp

--- CRA_Project_Tester/main.cpp
+++ CRA_Project_Tester/main.cpp
@@ -10,33 +10,30 @@ using namespace testing;
 #if (__DEBUG__ == 1)
 int main()
 {
-	testing::InitGoogleMock();
-	return RUN_ALL_TESTS();
+    testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
 }
 #else
 int main()
 {
-	ITestOperation* operation = nullptr;
-	while (1)
-	{
-		string command, param1, param2;
-
-		cin >> command;
-
-		cout << command << endl;
-		if (command == "read") {
-			cin >> param1;
-			operation = new Read;
-		}
-		else if (command == "help")
-			operation = new Help;
-		
-		else if (command == "exit")
-			break;
-		else
-			continue;
-		operation->run(param1, param2);
-	}
-	return 0;
+    ITestOperation* operation = nullptr;
+    while (1)
+    {
+        string command, param1, param2;
+        cin >> command;
+        cout << command << endl;
+        if (command == "read") {
+            cin >> param1;
+            operation = new Read;
+        }
+        else if (command == "help")
+	    operation = new Help;	
+	else if (command == "exit")
+	    break;
+        else
+            continue;
+        operation->run(param1, param2);
+    }
+    return 0;
 }
 #endif
