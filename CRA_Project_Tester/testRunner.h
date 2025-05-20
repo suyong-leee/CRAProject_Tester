a6147c3ff4960d5de4c8481dc59c3d21b423ebe0
ara.go <araking79@gmail.com>
2025-05-20 15:22:56 +0900

    [feat] testRunner.h에 Testsript4 추가

--- CRA_Project_Tester/testRunner.h
+++ CRA_Project_Tester/testRunner.h
@@ -23,6 +23,7 @@ public:
 		SCENARIO_1,
 		SCENARIO_2,
 		SCENARIO_3,
+		SCENARIO_4,
 		NUM_OF_OPERATOR,
 		PARAM_ZERO = NUM_OF_OPERATOR,
 	};
@@ -37,6 +38,7 @@ public:
 		operators[SCENARIO_1] = new SSDTest_FullWriteAndReadCompare(reinterpret_cast<Write*>(operators[OPERATOR_WRITE]), reinterpret_cast<Read*>(operators[OPERATOR_READ]));
 		operators[SCENARIO_2] = new SSDTest_PartialLBAWrite(reinterpret_cast<Write*>(operators[OPERATOR_WRITE]), reinterpret_cast<Read*>(operators[OPERATOR_READ]));
 		operators[SCENARIO_3] = new SSDTest_WriteReadAging(reinterpret_cast<Write*>(operators[OPERATOR_WRITE]), reinterpret_cast<Read*>(operators[OPERATOR_READ]));
+		operators[SCENARIO_4] = new SSDTest_EraseAndWriteAging;
 		operators[FULL_SCENARIO] = new SSDTest_FullScenario;
 	}
 	bool RunCommand()
@@ -90,6 +92,7 @@ private:
 		else if (command == "1_" || command == "1_FullWriteAndReadCompare") return SCENARIO_1;
 		else if (command == "2_" || command == "2_PartialLBAWrite") return SCENARIO_2;
 		else if (command == "3_" || command == "3_WriteReadAging") return SCENARIO_3;
+		else if (command == "4_" || command == "4_EraseAndWriteAging") return SCENARIO_4;
 		else if (command.find(".txt") != string::npos) return FULL_SCENARIO;
 		else return -1;
 	}
