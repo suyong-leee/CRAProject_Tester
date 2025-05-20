8a7d496f523a8b0bbbd18a777ff2a92798408ea1
yonghanlee <yonghanlee@samsung.com>
2025-05-20 13:28:56 +0900

    [Test Green]Logger Test Green 구현, print 후 생성된 파일의 내용 확인

--- CRA_Project_Tester/test_run.cpp
+++ CRA_Project_Tester/test_run.cpp
@@ -74,7 +74,39 @@ TEST_F(SSDTestFixture, FullWriteAndReadCompareTest) {
     ssd->run("", "");
 }
 
-TEST(LoggerTest, datacheck) {
-    Logger loggerInstance;
-    EXPECT_EQ(1, 0);
+class LoggerTest : public ::testing::Test {
+protected:
+    std::string logFilename = "test_log.txt";
+
+    void SetUp() override {
+        std::remove(logFilename.c_str());
+    }
+
+    void TearDown() override {
+        std::remove(logFilename.c_str());
+    }
+};
+
+TEST_F(LoggerTest, LogMessageIsWrittenToFile) {
+    Logger logger(logFilename);
+
+    std::string testMessage = "This is a test log entry.";
+    logger.print(1,"LoggerTestFunc", testMessage);
+
+    std::ifstream infile(logFilename);
+    ASSERT_TRUE(infile.is_open()) << "Log file could not be opened.";
+
+    std::string line;
+    bool found = false;
+
+    while (std::getline(infile, line)) {
+        if (line.find(testMessage) != std::string::npos &&
+            line.find("LoggerTestFunc( )") != std::string::npos) {
+            found = true;
+            break;
+        }
+    }
+
+    infile.close();
+    ASSERT_TRUE(found) << "Log message not found in log file.";
 }
